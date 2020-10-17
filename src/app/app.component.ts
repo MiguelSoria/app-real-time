e140b2796552631c5e1795f248aa495cbad2ae71
Miguel Soria <miguelsoriarangel@gmail.com>
2020-10-16 20:22:59 -0500

    Carga inicical del proyecto en el repositorio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { timeStamp } from 'console';
+import { MainTopBarMenuService } from './services/core/main-top-bar-menu.service';
 
+declare var App:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private menuSvc: MainTopBarMenuService){
+
+  }
   title = 'app-real-time';
+
+  miEmpresa: string="Grupo Modelo";
+  periodo: number=2020;
+  
+  menuItems:any[]=[];
+  
+  ngOnInit()
+  {
+    //load sidebar script
+    App.init();
+    //load Data
+    this.getData();
+  }
+getData(){
+  this.menuSvc.getItemsMenu().subscribe((data:any)=>{
+    this.menuItems= data;
+  });
+}
 }
